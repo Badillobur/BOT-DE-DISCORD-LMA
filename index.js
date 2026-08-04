5a779c83dfe440d322d3b11329c17b677b66e2a3
Administrator <admin@example.com>
2026-08-04 15:38:04 -0500

    Fix: remove disallowed intents

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessageReactions
+        GatewayIntentBits.GuildMembers
     ]
 });
 
